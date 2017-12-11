bf4dc2ec07f848dc71bd521e64da24381dad0000
Eva Ženatová <zenatovae@gmail.com>
2017-12-11 19:29:17 +0100

    Zmeny

--- asteroids.py
+++ asteroids.py
@@ -1,7 +1,11 @@
 import pyglet
-spaceship_imp = pyglet.image.load("resources/PNG/playerShip2_red.png")
 
 window = pyglet.window.Window()
+pressed_keys = set()
+
+spaceship_imp = pyglet.image.load("resources/PNG/playerShip2_red.png")
+spaceship_imp.anchor_x = spaceship_imp.width //2
+spaceship_imp.anchor_y = spaceship_imp.height //2
 
 class Spaceship:
     def __init__(self):
@@ -16,11 +20,17 @@ class Spaceship:
     def draw(self):
         self.sprite.x = self.x
         self.sprite.y = self.y
+        self.sprite.rotation = self.rotation
         self.sprite.draw()
 
     def tick(self, dt):
         self.x += self.x_speed * dt
         self.y += self.y_speed * dt
+        if pyglet.window.key.LEFT in pressed_keys:
+            self.rotation -=4
+        if pyglet.window.key.RIGHT in pressed_keys:
+            self.rotation +=4
+    #def move
 
 spaceship = Spaceship()
 
@@ -33,8 +43,18 @@ def draw():
     window.clear()
     spaceship.draw()
 
+def key_press(symbol, mod):
+    pressed_keys.add(symbol)
+    print(pressed_keys)
+
+def key_release(symbol, mod):
+    pressed_keys.discard(symbol)
+    print(pressed_keys)
+
 window.push_handlers(
     on_draw=draw,
+    on_key_press=key_press,
+    on_key_release=key_release,
 )
 
 pyglet.app.run()
